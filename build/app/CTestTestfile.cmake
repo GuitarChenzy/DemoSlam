131370f59e8e2b132e9bd22899c52222b078e272
GuitarChenzy <602974311@qq.com>
2021-05-08 09:31:59 +0800

    delete

--- build/app/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/czy/Projects/MySlam/app
-# Build directory: /home/czy/Projects/MySlam/build/app
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/src/CMakeFiles/SLAM.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "../../lib/libSLAM.a"
-)
